c7f30b70498179e754e55bba1418a858f36eff14
unknown <bedinellisofi@gmail.com>
2026-01-26 11:24:06 -0300

    fix github pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ReactCoder62310/'
+  base: '/ReactCoder62310/', // 👈 NOMBRE EXACTO DEL REPO
 })
